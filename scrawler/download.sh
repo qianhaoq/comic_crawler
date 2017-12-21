73792a95382d13062d6b99dc6671c657826493e3
qianhaoq <362609254@qq.com>
2017-12-21 21:48:14 +0800

    并发下载

--- scrawler/download.sh
+++ scrawler/download.sh
@@ -8,21 +8,22 @@ set -x
 
 # banwagong服务器，可直接下载
 wget -c -r ftp://67.218.159.92/ -P /home/qh/git/comic_crawler/scrawler/rawData/ &
-wait
-echo "step 1 done"
+# wait
+# echo "step 1 done"
 
 # vultr服务器，可直接下载
 wget -c -r ftp://45.77.126.21/ -P /home/qh/git/comic_crawler/scrawler/rawData/ &
-/usr/bin/proxychains wget -c -r ftp://139.162.113.67/ -P /home/qh/git/comic_crawler/scrawler/rawData/ &
+wget -c -r ftp://207.246.122.70/ -P /home/qh/git/comic_crawler/scrawler/rawData/ &
 
 # 等待下载完毕
-wait
-echo "step 2 done"
+# wait
+# echo "step 2 done"
 
 # linode日本服务器，不可直接下载，要用proxychains
 /usr/bin/proxychains wget -c -r ftp://45.33.37.117/ -P /home/qh/git/comic_crawler/scrawler/rawData/ &
-wget -c -r ftp://207.246.122.70/ -P /home/qh/git/comic_crawler/scrawler/rawData/ &
-wait
+/usr/bin/proxychains wget -c -r ftp://139.162.113.67/ -P /home/qh/git/comic_crawler/scrawler/rawData/ &
+
+# wait
 touch "compelte!"
 # 45.77.126.21  22 Los Angeles
 # 207.246.122.70 22 New Jersey
